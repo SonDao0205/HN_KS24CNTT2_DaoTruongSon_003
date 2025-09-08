a96fe632b0b0c8b6d9ecba8fc43aea3db97d65a7
Dao Truong Son <daotruongson2006@gmail.com>
2025-09-08 09:20:35 +0700

    003

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -4,42 +4,52 @@ export default function Pagination({ length }: { length: number }) {
   const currentPage = 1;
   return (
     <div className="d-flex justify-content-end align-items-center">
-      <p className="">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="30px"
-          viewBox="0 -960 960 960"
-          width="30px"
-          fill="#d1d1d1"
-        >
-          <path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z" />
-        </svg>
-      </p>
-      {Array.from({ length: pagesNumber }, (_, index) => {
-        const isActive = index + 1 === currentPage;
-
-        return (
-          <p
-            key={index + 1}
-            className={`py-2 px-3 rounded ${
-              isActive ? "border border-primary text-primary" : "text-body"
-            }`}
-          >
-            {index + 1}
+      <div className="d-flex gap-3">
+        <div className="changePage d-flex gap-2 align-items-center">
+          <p style={{ cursor: "pointer" }}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="30px"
+              viewBox="0 -960 960 960"
+              width="30px"
+              fill="#d1d1d1"
+            >
+              <path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z" />
+            </svg>
           </p>
-        );
-      })}
-      <p>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="30px"
-          viewBox="0 -960 960 960"
-          width="30px"
-          fill="#d1d1d1"
-        >
-          <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z" />
-        </svg>
-      </p>
+          {Array.from({ length: pagesNumber }, (_, index) => {
+            const isActive = index + 1 === currentPage;
+
+            return (
+              <p
+                style={{ cursor: "pointer" }}
+                key={index + 1}
+                className={`py-2 px-3 rounded ${
+                  isActive ? "border border-primary text-primary" : "text-body"
+                }`}
+              >
+                {index + 1}
+              </p>
+            );
+          })}
+          <p style={{ cursor: "pointer" }}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="30px"
+              viewBox="0 -960 960 960"
+              width="30px"
+              fill="#d1d1d1"
+            >
+              <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z" />
+            </svg>
+          </p>
+        </div>
+        <select className="form-select" style={{ height: "fit-content" }}>
+          <option value="">3 / Page</option>
+          <option value="">4 / Page</option>
+          <option value="">5 / Page</option>
+        </select>
+      </div>
     </div>
   );
 }
